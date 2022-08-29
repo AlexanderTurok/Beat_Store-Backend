b06af0cfe29836a44aa606ed48304cf2efe7490e
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-29 14:35:03 -0500

    craete handler constructor

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -5,6 +5,10 @@ import "github.com/gin-gonic/gin"
 type Handler struct {
 }
 
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
 
