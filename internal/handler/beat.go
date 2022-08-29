f2ae88a53945aeeb44f989511d6522962444de0a
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-29 15:56:00 -0500

    lay beat handler

---
+++ internal/handler/beat.go
@@ -0,0 +1,23 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) addBeat(ctx *gin.Context) {
+
+}
+
+func (h *Handler) getBeatById(ctx *gin.Context) {
+
+}
+
+func (h *Handler) getAllBeats(ctx *gin.Context) {
+
+}
+
+func (h *Handler) updateBeat(ctx *gin.Context) {
+
+}
+
+func (h *Handler) deleteBeat(ctx *gin.Context) {
+
+}
