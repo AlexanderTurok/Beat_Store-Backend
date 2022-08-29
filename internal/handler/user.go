7db84e87788e43965a4a93f37ecc66dfc6cb4571
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-29 15:55:47 -0500

    lay user handler

---
+++ internal/handler/user.go
@@ -0,0 +1,19 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) getUserById(ctx *gin.Context) {
+
+}
+
+func (h *Handler) getAllUsers(ctx *gin.Context) {
+
+}
+
+func (h *Handler) updateUser(ctx *gin.Context) {
+
+}
+
+func (h *Handler) deleteUser(ctx *gin.Context) {
+
+}
