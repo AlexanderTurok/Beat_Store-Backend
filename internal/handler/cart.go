93a8b7af6a62ab909235e4c0dce7a2952edc69ae
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-29 15:56:10 -0500

    lay cart handler

---
+++ internal/handler/cart.go
@@ -0,0 +1,23 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) addBeatToCart(ctx *gin.Context) {
+
+}
+
+func (h *Handler) getBeatByIdFromCart(ctx *gin.Context) {
+
+}
+
+func (h *Handler) getAllBeatsFromCart(ctx *gin.Context) {
+
+}
+
+func (h *Handler) deleteBeatInCart(ctx *gin.Context) {
+
+}
+
+func (h *Handler) deleteAllBeatsInCart(ctx *gin.Context) {
+
+}
