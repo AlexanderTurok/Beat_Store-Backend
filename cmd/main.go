625b486871d05910d5376702f8eecedee22c3fad
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-29 14:35:53 -0500

    start handler with server

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"github.com/AlexanderTurok/beat-store-backend/internal/handler"
 	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
 	"github.com/sirupsen/logrus"
 )
 
 func main() {
 
+	handler := handler.NewHandler()
+
 	server := new(beatstore.Server)
-	if err := server.Run("8000", nil); err != nil {
+	if err := server.Run("8000", handler.InitRoutes()); err != nil {
 		logrus.Fatalf("error while running the server: %s", err)
 	}
 }
