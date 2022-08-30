c09144081f86b7a434957f2ac536b40473c04233
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-29 21:01:46 -0500

    add users_beat const

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -1,13 +1,15 @@
 package repository
 
 import (
-	"database/sql"
 	"fmt"
+
+	"github.com/jmoiron/sqlx"
 )
 
 const (
-	beatTable = "beat"
-	userTable = "user"
+	beatTable      = "beat"
+	userTable      = "user"
+	usersBeatTable = "users_beat"
 )
 
 type Config struct {
@@ -19,8 +21,8 @@ type Config struct {
 	SSLMode  string
 }
 
-func NewPostgresDB(cfg Config) (*sql.DB, error) {
-	db, err := sql.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
 	if err != nil {
 		return nil, err
