f4c65fff539b3157738c3025cf16542a984f3ccd
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-29 19:06:18 -0500

    craete sign-up handler

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,11 +1,31 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func (h *Handler) signUp(ctx *gin.Context) {
+	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
+	"github.com/gin-gonic/gin"
+)
 
+func (h *Handler) signUp(c *gin.Context) {
+	var userInput beatstore.User
+
+	if err := c.BindJSON(userInput); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid input body: "+err.Error())
+		return
+	}
+
+	id, err := h.service.Authorization.CreateUser(userInput)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
-func (h *Handler) signIn(ctx *gin.Context) {
+func (h *Handler) signIn(c *gin.Context) {
 
 }
