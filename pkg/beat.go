f8ecfbad1583fdd2899ebddf5b8710f36d362096
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-30 14:27:52 -0500

    delete and update beat

--- pkg/beat.go
+++ pkg/beat.go
@@ -1,10 +1,29 @@
 package beatstore
 
+import "errors"
+
 type Beat struct {
-	Id    int     `json:"id" db:"id"`
-	Bpm   string  `json:"bpm" db:"bpm"`
-	Key   string  `json:"key" db:"key"`
-	Path  string  `json:"path" db:"path"`
-	Tag   string  `json:"tag" db:"tag"`
-	Price float32 `json:"price" db:"price"`
+	Id    int    `json:"id" db:"id"`
+	Bpm   string `json:"bpm" db:"bpm"`
+	Key   string `json:"key" db:"key"`
+	Path  string `json:"path" db:"path"`
+	Tag   string `json:"tag" db:"tag"`
+	Price string `json:"price" db:"price"`
+}
+
+type BeatUpdateInput struct {
+	Bpm   *string `json:"bpm"`
+	Key   *string `json:"key"`
+	Path  *string `json:"path"`
+	Tag   *string `json:"tag"`
+	Price *string `json:"price"`
+}
+
+func (b *BeatUpdateInput) Validate() error {
+	if b.Bpm == nil && b.Key == nil &&
+		b.Path == nil && b.Tag == nil && b.Price == nil {
+		return errors.New("update structure has no values")
+	}
+
+	return nil
 }
