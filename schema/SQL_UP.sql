512c8284d5461212790e0796bff6f2ab2d7d0181
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-30 10:59:11 -0500

    change table name; change id type

--- schema/SQL_UP.sql
+++ schema/SQL_UP.sql
@@ -1,30 +1,29 @@
-CREATE TABLE user (
-  id PRIMARY KEY,
+CREATE TABLE users (
+  id SERIAL NOT NULL UNIQUE,
   name VARCHAR(255) NOT NULL,
   username VARCHAR(255) UNIQUE NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
-  password_hash VARCHAR(255) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE tag (
-  id PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  id SERIAL NOT NULL UNIQUE,
+  name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE beat (
-  id PRIMARY KEY,
+  id SERIAL NOT NULL UNIQUE,
   bpm INT NOT NULL,
   key VARCHAR(16) NOT NULL,
   path VARCHAR(255) NOT NULL,
-  tags [] INT REFERENCES tag(id) ON DELETE CASCADE NOT NULL,
-  price FLOAT NOT NULL,
+  tags INT REFERENCES tag(id) ON DELETE CASCADE NOT NULL,
+  price FLOAT NOT NULL
 );
 
 CREATE TABLE users_beat (
-  id PRIMARY KEY,
-  user_id INT REFERENCES user (id) ON DELETE CASCADE NOT NULL,
+  id SERIAL NOT NULL UNIQUE,
+  user_id INT REFERENCES users (id) ON DELETE CASCADE NOT NULL,
   beat_id INT REFERENCES beat (id) ON DELETE CASCADE NOT NULL,
-  bought BOOLEAN DEFAULT (FALSE),
-  -- if bought=false -> it is beat which user sells
+  bought BOOLEAN DEFAULT (FALSE) -- if bought=false -> it is beat which user sells
   -- if bought=true -> it is beat which user bought 
 );
