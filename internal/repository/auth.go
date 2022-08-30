cb805904cc309ae221d730a27c35720c4dffa590
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-29 21:02:09 -0500

    create new beat

--- internal/repository/auth.go
+++ internal/repository/auth.go
@@ -1,17 +1,17 @@
 package repository
 
 import (
-	"database/sql"
 	"fmt"
 
 	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
+	"github.com/jmoiron/sqlx"
 )
 
 type AuthRepository struct {
-	db *sql.DB
+	db *sqlx.DB
 }
 
-func NewAuthService(db *sql.DB) *AuthRepository {
+func NewAuthService(db *sqlx.DB) *AuthRepository {
 	return &AuthRepository{
 		db: db,
 	}
@@ -20,7 +20,7 @@ func NewAuthService(db *sql.DB) *AuthRepository {
 func (r *AuthRepository) CreateUser(user beatstore.User) (int, error) {
 	var id int
 	query := fmt.Sprintf("INSERT INTO %s (name, username, email, password_hash) values ($1, $2, $3) RETURNING id", userTable)
-	err := r.db.QueryRow(query, user.Name, user.Username, user.Password).Scan(&id)
+	err := r.db.Get(&id, query, user.Name, user.Username, user.Password)
 
 	return id, err
 }
