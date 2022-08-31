c6da7681fd52c8694bc10a0804e742c48a2904d6
Alexander Turok <alexander.turok2004@gmail.com>
2022-08-30 20:27:29 -0500

    create CRUD hadnlers for user route

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,19 +1,70 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func (h *Handler) getUserById(ctx *gin.Context) {
+	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
+	"github.com/gin-gonic/gin"
+)
 
+func (h *Handler) getUser(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	user, err := h.service.User.Get(userId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
-func (h *Handler) getAllUsers(ctx *gin.Context) {
+func (h *Handler) getAllUsers(c *gin.Context) {
+	users, err := h.service.User.GetAll()
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	c.JSON(http.StatusOK, users)
 }
 
-func (h *Handler) updateUser(ctx *gin.Context) {
+func (h *Handler) updateUser(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	var userInput beatstore.UserUpdateInput
+	if err := c.BindJSON(&userInput); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.service.User.Update(userId, userInput); err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, statusResponse{"ok"})
 }
 
-func (h *Handler) deleteUser(ctx *gin.Context) {
+func (h *Handler) deleteUser(c *gin.Context) {
+	userId, err := getUserId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	if err := h.service.User.Delete(userId); err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, statusResponse{"ok"})
 }
