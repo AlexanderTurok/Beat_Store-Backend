a237e605ab4e613e91a05e26ac28d4de311de63f
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 15:10:28 -0500

    little fixes

--- pkg/beat.go
+++ pkg/beat.go
@@ -19,7 +19,7 @@ type Beat struct {
 	Mood      string    `json:"mood" db:"mood"`
 	Tags      []Tag     `json:"tags" db:"tags" binding:"required"`
 	Price     Price     `json:"price" db:"price" binding:"required"`
-	CreatedAt time.Time `json:"craeted_at" db:"created_at" binding:"required"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
 
 type Tag struct {
@@ -29,30 +29,30 @@ type Tag struct {
 }
 
 type Price struct {
-	Id             int    `json:"-"`
+	Id             int    `json:"-" db:"id"`
+	BeatId         int    `json:"beat_id" db:"beat_id"`
 	StandartPrice  string `json:"standart" db:"standart"`
 	PremiumPrice   string `json:"premium" db:"premium"`
 	UnlimitedPrice string `json:"unlimited" db:"unlimited"`
 }
 
 type BeatUpdateInput struct {
-	Name      *string    `json:"name"`
-	Bpm       *string    `json:"bpm"`
-	Key       *string    `json:"key"`
-	PhotoPath *string    `json:"photo_path"`
-	MP3Path   *string    `json:"mp3_path"`
-	WavPath   *string    `json:"wav_path"`
-	Genre     *string    `json:"genre"`
-	Mood      *string    `json:"mood"`
-	Tags      *[]Tag     `json:"tags"`
-	Price     *Price     `json:"price"`
-	CreatedAt *time.Time `json:"craeted_at"`
+	Name      *string `json:"name"`
+	Bpm       *string `json:"bpm"`
+	Key       *string `json:"key"`
+	PhotoPath *string `json:"photo_path"`
+	MP3Path   *string `json:"mp3_path"`
+	WavPath   *string `json:"wav_path"`
+	Genre     *string `json:"genre"`
+	Mood      *string `json:"mood"`
+	Tags      *[]Tag  `json:"tags"`
+	Price     *Price  `json:"price"`
 }
 
 func (b *BeatUpdateInput) Validate() error {
 	if b.Name == nil && b.Bpm == nil && b.Key == nil && b.PhotoPath == nil &&
-		b.MP3Path == nil && b.WavPath == nil && b.Tags == nil && b.Price == nil &&
-		b.Genre == nil && b.Mood == nil && b.CreatedAt == nil {
+		b.MP3Path == nil && b.WavPath == nil && b.Tags == nil && b.Genre == nil &&
+		b.Mood == nil && b.Price == nil {
 		return errors.New("update structure has no values")
 	}
 
