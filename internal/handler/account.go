9481652305bcc9b232416291f47e25e2f9bebbb7
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 12:44:21 -0500

    create UpdateAccount  handler

--- internal/handler/account.go
+++ internal/handler/account.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"net/http"
 
+	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
 	"github.com/gin-gonic/gin"
 )
 
@@ -23,24 +24,24 @@ func (h *Handler) getAccountByToken(c *gin.Context) {
 }
 
 func (h *Handler) updateAccount(c *gin.Context) {
-	// userId, err := getUserId(c)
-	// if err != nil {
-	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
-	// 	return
-	// }
+	accountId, err := getAccountId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
-	// var userInput beatstore.AccountUpdateInput
-	// if err := c.BindJSON(&userInput); err != nil {
-	// 	newErrorResponse(c, http.StatusBadRequest, err.Error())
-	// 	return
-	// }
+	var accountInput beatstore.AccountUpdateInput
+	if err := c.BindJSON(&accountInput); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
 
-	// if err := h.service.User.Update(userId, userInput); err != nil {
-	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
-	// 	return
-	// }
+	if err := h.service.Account.Update(accountId, accountInput); err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
-	// c.JSON(http.StatusOK, statusResponse{"ok"})
+	c.JSON(http.StatusOK, statusResponse{"ok"})
 }
 
 func (h *Handler) deleteAccount(c *gin.Context) {
