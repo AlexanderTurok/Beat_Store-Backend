7792472db15822af34a8a814f797b4b2c6bc481d
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 16:05:25 -0500

    create update handler

--- internal/handler/artist_beat.go
+++ internal/handler/artist_beat.go
@@ -1,31 +1,34 @@
 package handler
 
 import (
+	"net/http"
+
+	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) createBeat(c *gin.Context) {
-	// userId, err := getUserId(c)
-	// if err != nil {
-	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
-	// 	return
-	// }
+	artistId, err := getAccountId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
-	// var userInput beatstore.Beat
-	// if err := c.BindJSON(&userInput); err != nil {
-	// 	newErrorResponse(c, http.StatusBadRequest, err.Error())
-	// 	return
-	// }
+	var input beatstore.Beat
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
 
-	// id, err := h.service.Beat.Create(userId, userInput)
-	// if err != nil {
-	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
-	// 	return
-	// }
+	id, err := h.service.Beat.Create(artistId, input)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
-	// c.JSON(http.StatusOK, map[string]interface{}{
-	// 	"id": id,
-	// })
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) getAllArtistsBeats(c *gin.Context) {
