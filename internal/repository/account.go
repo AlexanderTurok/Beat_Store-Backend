36ab6c584cfdf6e69e1b5f80c66ffe9eb652e755
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 12:45:27 -0500

    craete update account method

--- internal/repository/account.go
+++ internal/repository/account.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"fmt"
+	"strings"
 
 	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
 	"github.com/jmoiron/sqlx"
@@ -24,4 +25,47 @@ func (r *AccountRepository) Get(accountId int) (beatstore.Account, error) {
 	err := r.db.Get(&account, query, accountId)
 
 	return account, err
+}
+
+func (r *AccountRepository) Update(accountId int, input beatstore.AccountUpdateInput) error {
+	query, args := createAccountUpdateQuery(accountId, input)
+	_, err := r.db.Exec(query, args...)
+
+	return err
+}
+
+func createAccountUpdateQuery(accountId int, input beatstore.AccountUpdateInput) (string, []interface{}) {
+	setValues := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if input.Name != nil {
+		setValues = append(setValues, fmt.Sprintf("name=$%d", argId))
+		args = append(args, *input.Name)
+		argId++
+	}
+
+	if input.Username != nil {
+		setValues = append(setValues, fmt.Sprintf("username=$%d", argId))
+		args = append(args, *input.Username)
+		argId++
+	}
+
+	if input.Email != nil {
+		setValues = append(setValues, fmt.Sprintf("email=$%d", argId))
+		args = append(args, *input.Email)
+		argId++
+	}
+
+	if input.PhotoPath != nil {
+		setValues = append(setValues, fmt.Sprintf("photo_path=$%d", argId))
+		args = append(args, *input.PhotoPath)
+		argId++
+	}
+
+	setQuery := strings.Join(setValues, ", ")
+	query := fmt.Sprintf("UPDATE %s SET %s WHERE id=$%d", accountTable, setQuery, argId)
+	args = append(args, accountId)
+
+	return query, args
 }
