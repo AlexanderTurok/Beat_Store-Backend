987480b0b00eb4abd58c516968c56287bfb0f4df
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 10:12:40 -0500

    comment old code

--- internal/handler/account.go
+++ internal/handler/account.go
@@ -1,70 +1,67 @@
 package handler
 
 import (
-	"net/http"
-
-	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) getAccount(c *gin.Context) {
-	userId, err := getUserId(c)
-	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
-		return
-	}
+	// userId, err := getUserId(c)
+	// if err != nil {
+	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	// 	return
+	// }
 
-	user, err := h.service.User.Get(userId)
-	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
-		return
-	}
+	// user, err := h.service.User.Get(userId)
+	// if err != nil {
+	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	// 	return
+	// }
 
-	c.JSON(http.StatusOK, user)
+	// c.JSON(http.StatusOK, user)
 }
 
 func (h *Handler) getAllAccounts(c *gin.Context) {
-	users, err := h.service.User.GetAll()
-	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
-		return
-	}
+	// users, err := h.service.User.GetAll()
+	// if err != nil {
+	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	// 	return
+	// }
 
-	c.JSON(http.StatusOK, users)
+	// c.JSON(http.StatusOK, users)
 }
 
 func (h *Handler) updateAccount(c *gin.Context) {
-	userId, err := getUserId(c)
-	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
-		return
-	}
+	// userId, err := getUserId(c)
+	// if err != nil {
+	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	// 	return
+	// }
 
-	var userInput beatstore.UserUpdateInput
-	if err := c.BindJSON(&userInput); err != nil {
-		newErrorResponse(c, http.StatusBadRequest, err.Error())
-		return
-	}
+	// var userInput beatstore.AccountUpdateInput
+	// if err := c.BindJSON(&userInput); err != nil {
+	// 	newErrorResponse(c, http.StatusBadRequest, err.Error())
+	// 	return
+	// }
 
-	if err := h.service.User.Update(userId, userInput); err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
-		return
-	}
+	// if err := h.service.User.Update(userId, userInput); err != nil {
+	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	// 	return
+	// }
 
-	c.JSON(http.StatusOK, statusResponse{"ok"})
+	// c.JSON(http.StatusOK, statusResponse{"ok"})
 }
 
 func (h *Handler) deleteAccount(c *gin.Context) {
-	userId, err := getUserId(c)
-	if err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
-		return
-	}
+	// userId, err := getUserId(c)
+	// if err != nil {
+	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	// 	return
+	// }
 
-	if err := h.service.User.Delete(userId); err != nil {
-		newErrorResponse(c, http.StatusInternalServerError, err.Error())
-		return
-	}
+	// if err := h.service.User.Delete(userId); err != nil {
+	// 	newErrorResponse(c, http.StatusInternalServerError, err.Error())
+	// 	return
+	// }
 
-	c.JSON(http.StatusOK, statusResponse{"ok"})
+	// c.JSON(http.StatusOK, statusResponse{"ok"})
 }
