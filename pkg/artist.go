c2fcfc4472115f649085cb5e3764aea4364ff046
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 10:16:06 -0500

    update

--- pkg/artist.go
+++ pkg/artist.go
@@ -3,7 +3,6 @@ package beatstore
 import "time"
 
 type Artist struct {
-	Id         int       `json:"-" db:"id"`
-	ArtistName string    `json:"artist_name" db:"artist_name" binding:"required"`
-	CreatedAt  time.Time `json:"craeted_at" db:"created_at" binding:"required"`
+	Id        int       `json:"-"`
+	CreatedAt time.Time `json:"craeted_at" binding:"required"`
 }
