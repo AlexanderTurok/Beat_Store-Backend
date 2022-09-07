e76eff71bdf6ed10d7ad5c75323d36858e48097d
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 12:46:01 -0500

    create update account method

--- internal/service/account.go
+++ internal/service/account.go
@@ -18,3 +18,11 @@ func NewAccountService(repos repository.Account) *AccountService {
 func (s *AccountService) Get(accountId int) (beatstore.Account, error) {
 	return s.repos.Get(accountId)
 }
+
+func (s *AccountService) Update(accountId int, input beatstore.AccountUpdateInput) error {
+	if err := input.Validate(); err != nil {
+		return err
+	}
+
+	return s.repos.Update(accountId, input)
+}
