9ebe33dff487b2a004e358e8873429b62aa888c9
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 10:14:14 -0500

    create constants for table names

---
+++ internal/repository/constants.go
@@ -0,0 +1,16 @@
+package repository
+
+const (
+	accountTable = "account"
+	artistTable  = "artist"
+
+	accountBeatTable     = "account_beat"
+	accountPlaylistTable = " account_playlist"
+
+	playlistBeatTable = "playlist_beat"
+	playlistTable     = "playlist"
+
+	beatTable  = "beat"
+	tagTable   = "tag"
+	priceTable = "price"
+)
