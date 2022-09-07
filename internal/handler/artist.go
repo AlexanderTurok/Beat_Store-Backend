0deb002db5138858306ab77ae85823ec0cc17ce5
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 13:52:32 -0500

    create handler for artist creation

--- internal/handler/artist.go
+++ internal/handler/artist.go
@@ -1,9 +1,24 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) createArtist(c *gin.Context) {
+	accountId, err := getAccountId(c)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	if err := h.service.Artist.Create(accountId); err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, statusResponse{"ok"})
 }
 
 func (h *Handler) getArtistByToken(c *gin.Context) {
