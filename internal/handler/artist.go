863149ab55887c1b18993d6864048537487dfb18
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-07 09:33:59 -0500

    lay hanlers

--- internal/handler/artist.go
+++ internal/handler/artist.go
@@ -1,1 +1,19 @@
 package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) createArtist(c *gin.Context) {
+
+}
+
+func (h *Handler) getArtist(c *gin.Context) {
+
+}
+
+func (h *Handler) getAllArtists(c *gin.Context) {
+
+}
+
+func (h *Handler) deleteArtist(c *gin.Context) {
+
+}
