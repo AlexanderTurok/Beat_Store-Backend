538ecfc1ba9e8572f385ca85aea73b0342dfe6bb
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-08 18:32:00 -0500

    create: getBeatById handler

--- internal/handler/api_beat.go
+++ internal/handler/api_beat.go
@@ -1,11 +1,28 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) getAllBeats(c *gin.Context) {
 
 }
 
 func (h *Handler) getBeatById(c *gin.Context) {
+	beatId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id")
+		return
+	}
 
+	beat, err := h.service.Beat.Get(beatId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, beat)
 }
