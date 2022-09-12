0a333706ddf8df67f70a9f472a8fac4f16f18e41
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-12 11:06:46 -0500

    update

--- pkg/account.go
+++ pkg/account.go
@@ -6,29 +6,31 @@ import (
 )
 
 type Account struct {
-	Id        int       `json:"-" db:"id"`
-	Name      string    `json:"name" db:"name" binding:"required"`
-	Username  string    `json:"username" db:"username" binding:"required"`
-	Email     string    `json:"email" db:"email" binding:"required"`
-	PhotoPath string    `json:"photo_path" db:"photo_path"`
-	Password  string    `json:"password" binding:"required"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	Id        int       `json:"-"`
+	Name      string    `json:"name"       binding:"required"`
+	Username  string    `json:"username"   binding:"required"`
+	Email     string    `json:"email"      binding:"required"`
+	PhotoPath string    `json:"photo_path"`
+	Confirmed bool      `json:"confirmed"`
+	Password  string    `json:"password"   binding:"required"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type AccountUpdateInput struct {
 	Name      *string `json:"name"`
 	Username  *string `json:"username"`
 	Email     *string `json:"email"`
+	Confirmed *string `json:"confirmed"`
 	PhotoPath *string `json:"photo_path"`
 }
 
 type AccountPassword struct {
-	Password string `json:"password" db:"password_hash"`
+	Password string `json:"password"`
 }
 
 func (a *AccountUpdateInput) Validate() error {
 	if a.Name == nil && a.Username == nil &&
-		a.PhotoPath == nil && a.Email == nil {
+		a.PhotoPath == nil && a.Email == nil && a.Confirmed == nil {
 		return errors.New("update strcture has no values")
 	}
 
