22ccf2a3368ea4f3b5216873679b3d627fe49d8f
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-13 09:45:21 -0500

    create playlist service

---
+++ internal/service/playlist.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"github.com/AlexanderTurok/beat-store-backend/internal/repository"
+	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
+)
+
+type PlaylistService struct {
+	repos *repository.Playlist
+}
+
+func NewPlaylistService(repos *repository.Playlist) *PlaylistService {
+	return &PlaylistService{
+		repos: repos,
+	}
+}
+
+func (s *PlaylistService) Create(accountId int, input beatstore.Playlist) error {
+	return s.repos.Create(accountId, input)
+}
