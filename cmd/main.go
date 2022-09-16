d6220d4366febf49f02673f61901e23695126db8
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-15 22:42:53 -0500

    refactor server

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"os"
+
 	"github.com/AlexanderTurok/beat-store-backend/internal/handler"
 	"github.com/AlexanderTurok/beat-store-backend/internal/repository"
 	"github.com/AlexanderTurok/beat-store-backend/internal/service"
-	beatstore "github.com/AlexanderTurok/beat-store-backend/pkg"
+	"github.com/AlexanderTurok/beat-store-backend/pkg/server"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -17,17 +19,17 @@ func main() {
 		logrus.Fatalf("error while loading enviroment variables: %s", err)
 	}
 
-	// if err := initConfig(); err != nil {
-	// 	logrus.Fatalf("error while initializing configs: %s", err)
-	// }
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error while initializing configs: %s", err)
+	}
 
 	db, err := repository.NewPostgresDB(repository.Config{
-		// Host:     viper.GetString("db.host"),
-		// Port:     viper.GetString("db.port"),
-		// Username: viper.GetString("db.username"),
-		// Password: os.Getenv("DB_Password"),
-		// DBName:   viper.GetString("db.dbname"),
-		// SSLMode:  viper.GetString("db.sslmode"),
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_Password"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
 	})
 	if err != nil {
 		logrus.Fatalf("error while starting postgres: s", err)
@@ -37,7 +39,7 @@ func main() {
 	service := service.NewService(repository)
 	handler := handler.NewHandler(service)
 
-	server := new(beatstore.Server)
+	server := new(server.Server)
 	if err := server.Run("8000", handler.InitRoutes()); err != nil {
 		logrus.Fatalf("error while running the server: %s", err)
 	}
