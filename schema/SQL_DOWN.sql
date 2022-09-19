24702979e0544e84e7409fcbb4ffd84dd30b1895
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-19 15:31:06 -0500

    create drop file

---
+++ schema/SQL_DOWN.sql
@@ -0,0 +1,15 @@
+DROP TABLE account_playlist;
+
+DROP TABLE playlist_beat;
+
+DROP TABLE playlist;
+
+DROP TABLE account_beat;
+
+DROP TABLE tag;
+
+DROP TABLE beat;
+
+DROP TABLE artist;
+
+DROP TABLE account;
