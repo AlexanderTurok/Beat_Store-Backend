099bd820bae9dbfbbf65b032967dd389cff8540e
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-19 15:21:12 -0500

    add db table names

---
+++ internal/repository/constants.go
@@ -0,0 +1,17 @@
+package repository
+
+// postgres - table names
+const (
+	accountTable = "account"
+	artistTable  = "artist"
+
+	accountBeatTable     = "account_beat"
+	accountPlaylistTable = " account_playlist"
+
+	playlistBeatTable = "playlist_beat"
+	playlistTable     = "playlist"
+
+	beatTable  = "beat"
+	tagTable   = "tag"
+	priceTable = "price"
+)
