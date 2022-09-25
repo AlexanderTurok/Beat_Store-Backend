4c62e6438ca6434e464d9cdf1694131ddae188b7
Alexander Turok <alexander.turok2004@gmail.com>
2022-09-24 21:27:39 -0500

    create payment service

--- internal/service/payment.go
+++ internal/service/payment.go
@@ -1,13 +1,28 @@
 package service
 
-import "github.com/AlexanderTurok/beat-store-backend/internal/repository"
+import (
+	"github.com/AlexanderTurok/beat-store-backend/internal/model"
+	"github.com/AlexanderTurok/beat-store-backend/internal/repository"
+	"github.com/AlexanderTurok/beat-store-backend/pkg/payment"
+)
 
 type PaymentService struct {
-	repos repository.Payment
+	repos   repository.Payment
+	payment payment.Payment
 }
 
-func NewPaymentService(repos repository.Payment) *PaymentService {
+func NewPaymentService(repos repository.Payment, payment payment.Payment) *PaymentService {
 	return &PaymentService{
-		repos: repos,
+		repos:   repos,
+		payment: payment,
 	}
+}
+
+func (s *PaymentService) CreatePaymentIntent(input model.PaymentInfo) (payment.PaymentIntent, error) {
+	paymentIntent, err := s.payment.CreatePaymentIntent(input.OrderAmount)
+	if err != nil {
+		return payment.PaymentIntent{}, err
+	}
+
+	return paymentIntent, nil
 }
